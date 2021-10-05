7059c36bdc38efee9271e6665f883dba077df823
Banin <banindm@mail.ru>
2021-10-05 15:30:26 +0300

    Добавлена генерация данных

--- js/main.js
+++ js/main.js
@@ -1,21 +1,119 @@
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const TITLES = [
+  'Квартира студия в престижном районе',
+  'Тихая квартирка недалеко от метро',
+  'Императорский дворец в центре Токио',
+];
+const PRICES = [25000, 100000];
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const ROOMS = [1, 4];
+const GUESTS = [1, 4];
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTIONS = [
+  'Комната в трёхкомнатной квартире, подойдёт молодым путешественникам.',
+  'У нас тут все ништяк. Ларек за углом. Шава 24 часа. Приезжайте! Интернетов нет!',
+  'Хейтеров просьба не беспокоить.',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 
+const LAT_BOUNDS = [
+  35.65000,
+  35.70000,
+];
+
+const LNG_BOUNDS = [
+  139.70000,
+  139.80000,
+];
+
+// функция для рандомного целого числа
 function getRandomInteger (min, max) {
   if (max<=min || min<0) {
     return 'Введено недопустимое значение';
   }
-  min = Math.ceil(min); // метод округляет число до целого в большую сторону
-  max = Math.floor(max); // метод округляет число до целого в меньшую сторону
-  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
-}
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-
-function getRandomFloatPoint (min, max, quantityNum) {
-  if (max<=min || min<0 || quantityNum<=0) {
+// функция для рандомного дробного числа
+function getRandomFloatPoint (min, max, digits = 5) {
+  if (max<=min || min<0 || digits<=0) {
     return 'Введено недопустимое значение';
   }
-  return (Math.random() * (max - min) + min).toFixed(quantityNum);
-}
+  return (Math.random() * (max - min) + min).toFixed(digits);
+};
 
-getRandomInteger (0, 10);
-getRandomFloatPoint (0, 10);
+// функция вывода номера изображения аватарки
+
+const getRandomAvatar = (avatar) => {
+  avatar = ("0" + getRandomInteger (0, 10)).slice(-2);
+  return 'img/avatars/user' + avatar + '.png'
+};
+
+
+// функция рандомного индекса массива строки
+
+const getRandomArrayElement = (elements) => {
+  return elements [getRandomInteger(0, elements.length - 1)];
+};
+
+
+// функция рандомных координат
+
+const createAdvertisement = () => {
+  const randomLat = getRandomFloatPoint(...LAT_BOUNDS);
+  const randomLng = getRandomFloatPoint(...LNG_BOUNDS);
+
+  return {
+    author: {
+      avatar: getRandomAvatar(),
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${randomLat}, ${randomLng}`,
+      price: getRandomInteger(...PRICES),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInteger(...ROOMS),
+      guests: getRandomInteger(...GUESTS),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: getRandomArrayElement(FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArrayElement(PHOTOS),
+    },
+    location: {
+      lat: randomLat,
+      lng: randomLng,
+    },
+  };
+};
+
+const objectGenerator = new Array(10).fill(null).map(createAdvertisement);
+console.log(objectGenerator);
