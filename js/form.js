1dd8e3712ad5a8574b8ac88dbe5bc2f90078882e
Banin <banindm@mail.ru>
2021-10-21 22:41:49 +0300

    добавлена валидация полей времени и цены

--- js/form.js
+++ js/form.js
@@ -1,12 +1,14 @@
 
 // код для валидации формы добавления объявления
 const AD_FORM = document.querySelector('.ad-form');
-const TITLE = document.querySelector('#title');
-const ROOM_NUMBER = document.querySelector('#room_number');
-const CAPACITY = document.querySelector('#capacity');
-//const PRICE = document.querySelector('#price');
-//const TYPE = document.querySelector('#type');
-//const ADDRESS = document.querySelector('#address');
+const TITLE = AD_FORM.querySelector('#title');
+const ROOM_NUMBER = AD_FORM.querySelector('#room_number');
+const CAPACITY = AD_FORM.querySelector('#capacity');
+const PRICE = AD_FORM.querySelector('#price');
+const TYPE = AD_FORM.querySelector('#type');
+const TIME_IN = AD_FORM.querySelector('#timein');
+const TIME_OUT = AD_FORM.querySelector('#timeout');
+//const ADDRESS = AD_FORM.querySelector('#address');
 
 const MIN_TEXT_LENGTH = 30;
 const MAX_TEXT_LENGTH = 100;
@@ -23,7 +25,9 @@ TITLE.addEventListener('input', () => {
   TITLE.reportValidity();
 });
 
-const ROOMS_GUESTS = {
+// Количество комнат - Количество мест
+
+const RoomsGuests = {
   1: ['1'],
   2: ['1', '2'],
   3: ['1', '2', '3'],
@@ -32,7 +36,7 @@ const ROOMS_GUESTS = {
 
 const changeRoomNumberValue = (value) => {
   [...CAPACITY.options].forEach((option) => {
-    option.disabled = !ROOMS_GUESTS[value].includes(option.value);
+    option.disabled = !RoomsGuests[value].includes(option.value);
   });
   CAPACITY.value = value > 3 ? '0' : value;
 };
@@ -43,22 +47,82 @@ ROOM_NUMBER.addEventListener('change', (evt) => {
   changeRoomNumberValue(evt.target.value);
 });
 
-AD_FORM.addEventListener('reset', (evt) => {
-  evt.preventDefault();
-  changeRoomNumberValue(ROOM_NUMBER.value);
-  window.pageActivate.getDeactivePage();
-  window.form.getResetForm();
+// Тип жилья - Цена за ночь
+
+const RoomsPrices = {
+  BUNGALOW: 0,
+  FLAT: 1000,
+  HOTEL: 3000,
+  HOUSE: 5000,
+  PALACE: 10000,
+};
+
+const changeHousingType = (value) => {
+  let roomPrice = 0;
+  switch (value) {
+    case 'bungalow':
+      roomPrice = RoomsPrices.BUNGALOW;
+      break;
+    case 'flat':
+      roomPrice = RoomsPrices.FLAT;
+      break;
+    case 'hotel':
+      roomPrice = RoomsPrices.HOTEL;
+      break;
+    case 'house':
+      roomPrice = RoomsPrices.HOUSE;
+      break;
+    case 'palace':
+      roomPrice = RoomsPrices.PALACE;
+      break;
+  }
+  PRICE.placeholder = roomPrice;
+};
+
+changeHousingType(TYPE.value);
+
+TYPE.addEventListener('change', (evt) => {
+  changeHousingType(evt.target.value);
+  if (TYPE.value === 'bungalow') {
+    PRICE.min = 0;
+  } else if (TYPE.value === 'flat') {
+    PRICE.min = 1000;
+  } else if (TYPE.value === 'hotel') {
+    PRICE.min = 3000;
+  } else if (TYPE.value === 'house') {
+    PRICE.min = 5000;
+  } else if (TYPE.value === 'palace') {
+    PRICE.min = 10000;
+  } else {
+    PRICE.setCustomValidity('');
+  }
+  PRICE.reportValidity();
 });
 
+// Время заезда, Время выезда
 
-// Количество комнат и количество мест
+const changeTimeOutValue = (value) => {
+  TIME_OUT.value = value;
+};
 
-// 1 комната — «для 1 гостя»;
-// 2 комнаты — «для 2 гостей» или «для 1 гостя»;
-// 3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;
-// 100 комнат — «не для гостей».
+const changeTimeInValue = (value) => {
+  TIME_IN.value = value;
+};
 
-// document.querySelector('form').onchange = function(e) {
-//   this.timein.value = e.target.value;
-//   this.timeout.value = e.target.value;
-// };
+changeTimeOutValue(TIME_IN.value);
+
+TIME_IN.addEventListener('change', (evt) => {
+  changeTimeOutValue(evt.target.value);
+});
+
+TIME_OUT.addEventListener('change', (evt) =>{
+  changeTimeInValue(evt.target.value);
+});
+
+TIME_IN.addEventListener('change', (evt) => {
+  changeTimeOutValue(evt.target.value);
+});
+
+TIME_OUT.addEventListener('change', (evt) =>{
+  changeTimeInValue(evt.target.value);
+});
